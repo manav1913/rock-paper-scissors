e81c61eacb99550204545c0480a821ed14e8e392
manav1913 <69932908+manav1913@users.noreply.github.com>
2026-04-13 08:55:12 -0700

    created updatescore function and fixed some css styling

--- index.js
+++ index.js
@@ -6,47 +6,67 @@ const scoreEl = document.getElementById("score")
 const resetEl = document.getElementById("reset-score")
 
 
-let score = {
+let score = JSON.parse(localStorage.getItem("score")) || {
     Wins : 0,
-    Tie : 0,
-    Looses : 0
+    Tie: 0,
+    Losses : 0
 }
 
-rockEl.addEventListener("click", function(){
-   playGame("Rock") 
+rockEl.addEventListener("click", function () {
+    playGame("Rock")
 })
 
-paperEl.addEventListener("click", function(){
+paperEl.addEventListener("click", function () {
     playGame("Paper")
 })
 
-scissorsEl.addEventListener("click", function(){
+scissorsEl.addEventListener("click", function () {
     playGame("Scissors")
 })
 
 
-resetEl.addEventListener("click",function(){
+resetEl.addEventListener("click", function () {
+    score.Wins = 0;
+    score.Losses = 0;
+    score.Tie = 0;
 
+    localStorage.removeItem("score");
+
+    updateScore();
+
+    resultEl.textContent = "Game reset. Play again!";
 })
 
 
-function playGame(playerMove){
-    let randomNumber = Math.floor(Math.random()*3)
-    let computerMove = randomNumber ===0 ? "Rock" :
-        randomNumber ===1 ? "Paper" : "Scissors"
+function updateScore() {
+    scoreEl.textContent = `Wins: ${score.Wins} | Losses: ${score.Losses} | Tie: ${score.Tie}`;
+}
+
+function playGame(playerMove) {
+    let randomNumber = Math.floor(Math.random() * 3)
+    let computerMove = randomNumber === 0 ? "Rock" :
+        randomNumber === 1 ? "Paper" : "Scissors"
 
 
-    if(playerMove === computerMove){
-        resultEl.textContent = `You Choose ${playerMove} Computer Chosse ${computerMove} So It's a Tie.`
-    }else if((playerMove==="Rock" && computerMove === "Scissors") ||
-        (playerMove === "Paper" && computerMove === "Rock" ) ||
-        (playerMove === "Scissors" && computerMove === "Paper")){
-            resultEl.textContent =  `You Choose ${playerMove} Computer Chosse ${computerMove} So You Won.`
-        }else{
-            resultEl.textContent =  `You Choose ${playerMove} Computer Chosse ${computerMove} So You Lost.`
-        }
-    
+    if (playerMove === computerMove) {
+        resultEl.textContent = `You : ${playerMove} | Computer : ${computerMove} | So => Tie.`
+        score.Tie++
 
-   
+    } else if ((playerMove === "Rock" && computerMove === "Scissors") ||
+        (playerMove === "Paper" && computerMove === "Rock") ||
+        (playerMove === "Scissors" && computerMove === "Paper")) {
+        resultEl.textContent = `You : ${playerMove} | Computer : ${computerMove} | So => Won.`
+        score.Wins++
+
+    } else {
+        resultEl.textContent = `You : ${playerMove} | Computer : ${computerMove} | So => Lost.`
+        score.Losses++
+    }
+
+    localStorage.setItem("score", JSON.stringify(score))
+    updateScore()
+
+
+
 }
 
